577b7f533da22a7a41745f12ead5f6ba95f888dd
MrFastFox666 <105514361+MrFastFox666@users.noreply.github.com>
2026-03-31 00:59:28 -0400

    Update fingerprints.py

--- opendbc_repo/opendbc/car/hyundai/fingerprints.py
+++ opendbc_repo/opendbc/car/hyundai/fingerprints.py
@@ -539,6 +539,27 @@ FW_VERSIONS = {
   },
   CAR.GENESIS_G80_2ND_GEN_FL: {
     (Ecu.fwdRadar, 0x7d0, None): [
+      b'\xf1\x00RG3_ SC -----      1.00 1.02 99110-T1120         ',
+      b'\xf1\x00RGev SC FHCUP      1.00 1.01 99110-JI010         ',
+      b'\xf1\x8b "\x0)',
+      b'\xf1\x10\x17\xdf\x01\x0',
+    ],
+    (Ecu.fwdCamera, 0x7c4, None): [
+      b'\xf1\x00RG3 MF  AT USA LHD 1.00 1.01 99211-T1200 230607',
+      b'\xf1\x00RG3EMF  AT USA LHD 1.00 1.01 99211-JI010 210419',
+      b'\xf1\x8b "\x05',
+    ],
+    (Ecu.cornerRadar, 0x7b7, None): [
+      b'\xf1\x00RG3 BC RR 1.00 , 1.01 (u5E"\x006\x00\t',
+      b'\xf1\x8b "\x02x05 "\x02\x05\x00\x00\x00\x00\x00\x00\x00\x00'
+    ],  
+    (Ecu.eps, 0x7d4, None): [
+      b'\xf1\x00RG3 MDS R 1.00 1.01 57700-JI400 1520',
+    ],
+
+  },
+  CAR.GENESIS_G80_EV_HDA: {
+    (Ecu.fwdRadar, 0x7d0, None): [
       b'\xf1\x00RG3_ SCC -----      1.00 1.02 99110-T1120         ',
       b'\xf1\x00RGev SCC FHCUP      1.00 1.01 99110-JI010         ',
       b'\xf1\x8b "\x04)',
@@ -549,14 +570,17 @@ FW_VERSIONS = {
       b'\xf1\x00RG3EMFC  AT USA LHD 1.00 1.01 99211-JI010 210419',
       b'\xf1\x8b "\x05&',
     ],
+    (Ecu.hvac, 0x7b3, None): [
+      b"\xf1\x00RG3 EV97255-JI301CONTROL ASS'Y-DATC  1.03RG3 EV DATC(-) 3.1   ",
+    ],
     (Ecu.cornerRadar, 0x7b7, None): [
       b'\xf1\x00RG3 BCW RR 1.00 , 1.01 (u5E"\x006\x00\t',
       b'\xf1\x8b "\x02\x05 "\x02\x05\x00\x00\x00\x00\x00\x00\x00\x00'
-    ],  
+    ],
     (Ecu.eps, 0x7d4, None): [
       b'\xf1\x00RG3 MDPS R 1.00 1.01 57700-JI400 1520',
     ],
-
+   
   },
 
   CAR.GENESIS_G90: {
