8b11f61592c9e7dcc042ef3aad84910325bcdc45
MrFastFox666 <105514361+MrFastFox666@users.noreply.github.com>
2026-03-31 14:37:50 -0400

    Update values.py

--- opendbc_repo/opendbc/car/hyundai/values.py
+++ opendbc_repo/opendbc/car/hyundai/values.py
@@ -602,9 +602,9 @@ class CAR(Platforms):
     flags=HyundaiFlags.EV | HyundaiFlags.CANFD_NO_RADAR_DISABLE,
   )
   GENESIS_G80_EV = HyundaiCanFDPlatformConfig(
-    [HyundaiCarDocs("Genesis G80 (2.5T Advanced Trim, with HDA II) 2024", "Highway Driving Assist II", car_parts=CarParts.common([CarHarness.hyundai_p]))],
-    CarSpecs(mass=2280, wheelbase=3.00, steerRatio=14.0),
-    flags=HyundaiFlags.EV | HyundaiFlags.CANFD_NO_RADAR_DISABLE,
+    [HyundaiCarDocs("Genesis G80 Electrified 2023", "Highway Driving Assist (HDA1)", car_parts=CarParts.common([CarHarness.hyundai_k]))],
+    CarSpecs(mass=2286, wheelbase=3.01, steerRatio=14.0),
+    flags=HyundaiFlags.EV,
   )
   GENESIS_G90 = HyundaiPlatformConfig(
     [HyundaiCarDocs("Genesis G90 2017-20", "All", car_parts=CarParts.common([CarHarness.hyundai_c]))],
