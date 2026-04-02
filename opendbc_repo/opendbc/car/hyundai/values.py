1427d6c3fd949eb4345988a450ecaaa9dcb5d65e
MrFastFox666 <105514361+MrFastFox666@users.noreply.github.com>
2026-04-02 09:40:28 -0400

    Update values.py

--- opendbc_repo/opendbc/car/hyundai/values.py
+++ opendbc_repo/opendbc/car/hyundai/values.py
@@ -600,7 +600,7 @@ class CAR(Platforms):
     # TODO: correct wheelbase + mass to finalized figures, assume it matches the 2019
     [HyundaiCarDocs("Genesis G80 Electrified 2023", "All", car_parts=CarParts.common([CarHarness.hyundai_k]))],
     CarSpecs(mass=2280, wheelbase=3.01, steerRatio=14.0),
-    flags=HyundaiFlags.CANFD_NO_RADAR_DISABLE | HyundaiFlags.EV,
+    flags=HyundaiFlags.EV,
   )
   GENESIS_G80_2ND_GEN_FL = HyundaiCanFDPlatformConfig(
     [HyundaiCarDocs("Genesis G80 (2.5T Advanced Trim, with HDA II) 2024", "Highway Driving Assist II", car_parts=CarParts.common([CarHarness.hyundai_p]))],
